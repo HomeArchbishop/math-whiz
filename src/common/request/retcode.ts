cedc4a71517caba5286ad9906e8b4f4006bb13bb
homearchbishop <homearchbishop@foxmail.com>
2025-10-31 15:17:21 +0800

    refactor: use Retcode as enum

--- src/common/request/retcode.ts
+++ src/common/request/retcode.ts
@@ -1,7 +1,7 @@
-export const Retcode = Object.freeze({
-  SUCCESS: 0,
-  NO_RESPONSE: 10000,
-})
+export enum Retcode {
+  SUCCESS = 0,
+  NO_RESPONSE = 10000,
+}
 
 export class RetcodeError extends Error {
   constructor (public readonly retcode: number, public readonly message: string) {
